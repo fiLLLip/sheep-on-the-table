f3f3d62846d49692a1e2fab0fc741d505edbe74c
Filip Andre Larsen Tomren <filiptomren@gmail.com>
2012-09-10 00:40:44 +0200

    Edited getSheepList(), added JavaDoc

--- src/my/sheeponthetable/tools/mysqlHelper.java
+++ src/my/sheeponthetable/tools/mysqlHelper.java
@@ -50,21 +50,24 @@ public class mysqlHelper {
         }
     }
     
+    /**
+     *
+     * @return
+     */
     public List<sheep> getSheepList() {
-        List<sheep> sheeps = new ArrayList<sheep>();
-        try{
-            ResultSet results = null;
+        try {
+            List<sheep> sheeps = new ArrayList<>();
+            ResultSet results;
             results = stmt.executeQuery("SELECT id, eier_id, navn, kommentar, fodt_ar FROM sau WHERE eier_id = 1");
             while(results.next()){
-                List<sheepUpdate> updates = new ArrayList<sheepUpdate>();
+                List<sheepUpdate> updates = new ArrayList<>();
                 sheeps.add(new sheep(Integer.parseInt(results.getString(1)), Integer.parseInt(results.getString(2)), results.getString(3), results.getString(4), Integer.parseInt(results.getString(5)), updates));
             }
             return sheeps;
+        } catch (SQLException ex) {
+            Logger.getLogger(mysqlHelper.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
         }
-        catch(SQLException e){
-            e.printStackTrace();
-        }
-        return sheeps;
     }
     
     private String urlGenerator(String host, int port, String database){
