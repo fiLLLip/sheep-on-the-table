85fc29fff54cece17a1da646a927e21492c29875
Filip Andre Larsen Tomren <filiptomren@gmail.com>
2012-09-10 10:42:30 +0200

    Added getSheepUpdates()

--- src/my/sheeponthetable/tools/mysqlHelper.java
+++ src/my/sheeponthetable/tools/mysqlHelper.java
@@ -60,10 +60,25 @@ public class mysqlHelper {
             ResultSet results;
             results = stmt.executeQuery("SELECT id, eier_id, navn, kommentar, fodt_ar FROM sau WHERE eier_id = 1");
             while(results.next()){
-                List<sheepUpdate> updates = new ArrayList<>();
+                List<sheepUpdate> updates = getSheepUpdates(Integer.parseInt(results.getString(1)));
                 sheeps.add(new sheep(Integer.parseInt(results.getString(1)), Integer.parseInt(results.getString(2)), results.getString(3), results.getString(4), Integer.parseInt(results.getString(5)), updates));
             }
             return sheeps;
+        } catch (SQLException ex) {
+            Logger.getLogger(mysqlHelper.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    private List<sheepUpdate> getSheepUpdates(int id) {
+        try {
+            List<sheepUpdate> updates = new ArrayList<>();
+            ResultSet results;
+            results = stmt.executeQuery("SELECT id, posisjon_x, posisjon_y, puls, temperatur, UNIX_TIMESTAMP(timestamp) FROM oppdateringer WHERE sau_id = " + Integer.toString(id) + " ORDER BY id DESC");
+            while(results.next()){
+                updates.add(new sheepUpdate(Integer.parseInt(results.getString(1)), Float.valueOf(results.getString(2).trim()), Float.valueOf(results.getString(3).trim()), Integer.parseInt(results.getString(4)), Integer.parseInt(results.getString(5)), Integer.parseInt(results.getString(6))));
+            }
+            return updates;
         } catch (SQLException ex) {
             Logger.getLogger(mysqlHelper.class.getName()).log(Level.SEVERE, null, ex);
             return null;
