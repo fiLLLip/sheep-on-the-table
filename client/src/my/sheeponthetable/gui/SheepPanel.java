f1ece2fd9a14579cb61d9e1b5e8d9d1ba8cb1607
Alexander Olsen <alex.j.olsen@gmail.com>
2012-09-12 12:15:56 +0200

    Emptyshell.gui
    Endret remove sheep buttin til å lede til warningbox

--- client/src/my/sheeponthetable/gui/SheepPanel.java
+++ client/src/my/sheeponthetable/gui/SheepPanel.java
@@ -71,6 +71,11 @@ public class SheepPanel extends javax.swing.JFrame {
         jButton2.setText("Add sheep");
 
         jButton3.setText("Remove sheep");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("ID");
 
@@ -133,6 +138,10 @@ public class SheepPanel extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+       new WarningBox().setVisible(true); // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
