68024c849ca5e6e67f17809e33699b6ca6a28e12
Håkon <hakonmaa@gmail.com>
2012-10-17 16:18:53 +0200

    Deleted main in FarmTools and UserProperties

--- client/src/my/sheeponthetable/gui/UserProperties.java
+++ client/src/my/sheeponthetable/gui/UserProperties.java
@@ -48,7 +48,7 @@ public class UserProperties extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+  /*  public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -73,12 +73,13 @@ public class UserProperties extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
+      /*  java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new UserProperties().setVisible(true);
             }
         });
-    }
+    }*/
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }
