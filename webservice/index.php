f5158e98530a6bfea40d79780766975f17b60534
Filip Andre Larsen Tomren <filiptomren@gmail.com>
2012-10-19 03:10:38 +0200

    Changed from a nonfunctional JSON-RPC 2.0 API to a new one that actually works and follows JSON-RPC 2.0 protocol described here: http://www.jsonrpc.org/specification

--- webservice/index.php
+++ webservice/index.php
@@ -1,11 +1,19 @@
 <?php
-require_once 'jsonRPCServer.php';
-require_once 'database.php';
-require 'sheep.php';
-require 'restrictedSheep.php';
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+	require_once("JsonRpcServer.php");
+	require_once("database.php");
+	require_once("sheep.php");
+    session_start();
+	/*
+	$server = new AuthenticatorJsonRpcServer(file_get_contents("php://input"));
 
-$mySheep = new RestrictedSheep();
-	
-jsonRPCServer::handle($mySheep)
-	or print 'no request';
+	$server->addService(new MathServiceImpl());
+	$server->processingRequests();
+
+	*/
+	$server = new JsonRpcServer(file_get_contents("php://input"));
+
+	$server->addService(new Sheep());
+	$server->processingRequests();
+}
 ?>
--- webservice/jsonRPCServer.php
+++ webservice/jsonRPCServer.php
@@ -1,85 +1,194 @@
 <?php
-/*
-					COPYRIGHT
+function __autoload($className) {
+    if(strstr($className,'Exception')) {
+        include_once("JsonRpcExceptions.php");
+    } else {
+        include_once($className.".php");
+    }
+}
 
-Copyright 2007 Sergio Vaccaro <sergio@inservibile.org>
+class JsonRpcServer {
+	private $_requestText;
+	private $_listOfCallableServices;
+	private $_requestObject;
+	private $_responseBatchArray = array();
 
-This file is part of JSON-RPC PHP.
-
-JSON-RPC PHP is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-JSON-RPC PHP is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with JSON-RPC PHP; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-/**
- * This class build a json-RPC Server 1.0
- * http://json-rpc.org/wiki/specification
- *
- * @author sergio <jsonrpcphp@inservibile.org>
- */
-class jsonRPCServer {
-	/**
-	 * This function handle a request binding it to a given object
-	 *
-	 * @param object $object
-	 * @return boolean
-	 */
-	public static function handle($object) {
-		
-		// checks if a JSON-RCP request has been received
-		if (
-			$_SERVER['REQUEST_METHOD'] != 'POST' || 
-			empty($_SERVER['CONTENT_TYPE']) ||
-			$_SERVER['CONTENT_TYPE'] != 'application/json'
-			) {
-			// This is not a JSON-RPC request
+	function __construct($postRequest) {
+		$this->_requestText = $postRequest;
+		$this->_listOfCallableServices = array();
+	}
+	public function addService($classInstance) {
+        array_push($this->_listOfCallableServices, $classInstance);
+	}
+	public function processingRequests() {
+		try {
+			$this->parseRequestJson();
+			$this->performCalls();
+		} catch(Exception $e) {
+			$responseBody = new RpcError($e->getMessage(),$e->getCode());
+			$responseObject = new RpcResponse($responseBody);
+			$this->doResponse($responseObject->getRpcResponseObject());
+		}
+	}
+	private function parseRequestJson() {
+		if(!is_null($requestObjects = json_decode($this->_requestText))) {
+			$this->_requestObject = $requestObjects;
+		} else {
+			throw new JsonRpcParseErrorException();
+		}
+	}
+	private function performCalls() {
+		if($this->isBatchRequestAndNotEmpty()) {
+			$this->performBatchCall();
+		} else {
+			$this->performSingleCall();
+		}
+	}
+	private function isBatchRequestAndNotEmpty() {
+		if(is_array($this->_requestObject)) {
+			if(empty($this->_requestObject)) {
+				throw new JsonRpcInvalidRequestException();
+			}
+			return true;
+		} else {
 			return false;
 		}
-				
-		// reads the input data
-		$request = json_decode(file_get_contents('php://input'),true);
-		
-		// executes the task on local object
-		try {
-			if ($result = @call_user_func_array(array($object,$request['method']),$request['params'])) {
-				$response = array (
-									'id' => $request['id'],
-									'result' => $result,
-									'error' => NULL
-									);
-			} else {
-				$response = array (
-									'id' => $request['id'],
-									'result' => NULL,
-									'error' => 'unknown method or incorrect parameters'
-									);
+	}
+	private function performBatchCall() {
+		foreach ($this->_requestObject as $request) {
+			$responseObject = $this->getResponseObject($request);
+			if (!$this->isNotification($request)) {
+				array_push($this->_responseBatchArray, $responseObject->getRpcResponseObject());
 			}
-		} catch (Exception $e) {
-			$response = array (
-								'id' => $request['id'],
-								'result' => NULL,
-								'error' => $e->getMessage()
-								);
 		}
-		
-		// output the response
-		if (!empty($request['id'])) { // notifications don't want response
-			header('content-type: text/javascript');
-			echo json_encode($response);
+		$this->doResponse($this->_responseBatchArray);
+	}
+	private function performSingleCall() {
+		$responseObject = $this->getResponseObject($this->_requestObject);
+		$obj = $responseObject->getRpcResponseObject();
+		if (!$this->isNotification($this->_requestObject)) {
+			$this->doResponse($obj);
 		}
-		
-		// finish
+	}
+	private function isNotification($requestObject) {
+		if(is_object($requestObject) && is_null($requestObject->id)) {
+			return true;
+		}
+	}
+	private function getResponseObject($requestObject) {
+		try {
+			$this->validateRequest($requestObject);
+			$methodOwnerService = $this->isMethodAvailable($requestObject);
+			$this->validateAndSortParameters($methodOwnerService, $requestObject);
+			$responseObject = $this->buildResponseObject($requestObject, $methodOwnerService);
+		} catch(JsonRpcMethodNotFoundException $exception) {
+			$responseObject = $this->buildResponseObject($exception);
+			$responseObject->setResponseObjectId($requestObject->id);
+		} catch(Exception $exception) {
+			$responseObject = $this->buildResponseObject($exception);
+		}
+		return $responseObject;
+	}
+	private function validateRequest($request) {
+		if(!$this->isValidRequestObject($request)) {
+			throw new JsonRpcInvalidRequestException();
+		} else {
+			return true;
+		}
+	}
+	private function isValidRequestObject($requestObject) {
+		return ($requestObject->jsonrpc == RpcResponse::VERSION
+		       && $this->isValidRequestObjectId($requestObject->id)
+			   && $this->isValidRequestObjectMethod($requestObject->method));
+	}
+	private function isValidRequestObjectId($requestId) {
+		return (is_null($requestId)
+		        || is_string($requestId)
+				// 2 and "2" is valid but 2.1 and "2.1" is not
+		        || (ctype_digit($requestId) xor is_int($requestId)));
+	}
+	private function isValidRequestObjectMethod($requestMethod) {
+		return (!is_null($requestMethod)
+			   && is_string($requestMethod)
+			   && strncmp($reserved = "rpc.",$requestMethod,strlen($reserved)));
+	}
+	protected function isMethodAvailable($requestObject) {
+        $length = count($this->_listOfCallableServices);
+        for($i=0;$i<$length;$i++) {
+			if(array_key_exists($requestObject->method, $this->_listOfCallableServices[$i]->getCallableMethodNames())) {
+    			return $this->_listOfCallableServices[$i];
+			}
+        }
+		throw new JsonRpcMethodNotFoundException();
+	}
+	private function validateAndSortParameters($methodOwnerService, $requestObject) {
+		$validParameters = $methodOwnerService->getCallableMethodParameters($requestObject->method);
+
+		if($this->isValidParamsNumber($validParameters, $requestObject)
+		   && $this->isValidParamsName($validParameters, $requestObject)) {
+
+			$this->setMethodParamsSequence($validParameters, $requestObject);
+		} else {
+			throw new JsonRpcInvalidParamsException();
+		}
+	}
+	private function setMethodParamsSequence($validParameters, $requestParameters) {
+		$sortedObject = new stdClass();
+		if(is_object($requestParameters->params)) {
+			foreach($validParameters as $parameter) {
+				$sortedObject->{$parameter->name} = $requestParameters->params->{$parameter->name};
+			}
+			$requestParameters->params = $sortedObject;
+		}
+	}
+	private function buildResponseObject($requestOrExceptionObject, $service = null) {
+		if(is_null($service)) {
+			$responseBody = new RpcError($requestOrExceptionObject->getMessage(),$requestOrExceptionObject->getCode());
+			$responseObject = new RpcResponse($responseBody);
+		} else {
+			$callbackResult = $this->call($service, $requestOrExceptionObject);
+			$responseObject = new RpcResponse($callbackResult, $requestOrExceptionObject->id);
+		}
+		return $responseObject;
+	}
+	private function isValidParamsNumber($validParameters, $requestObject) {
+		$validParameterCount = count($validParameters);
+		$requestParameterCount = $this->countRequestParams($requestObject->params);
+
+		if($validParameterCount != $requestParameterCount) {
+			return false;
+		}
 		return true;
+	}
+	private function countRequestParams($requestObjectParams) {
+		if(is_object($requestObjectParams)) {
+			return count(get_object_vars($requestObjectParams));
+		} else {
+			return count($requestObjectParams);
+		}
+	}
+	private function isValidParamsName($validParameters, $requestObject) {
+		if(is_object($requestObject->params)){
+			$requestParamNames = array_keys(get_object_vars($requestObject->params));
+			foreach($validParameters as $parameter) {
+				if(!in_array($parameter->name, $requestParamNames, true)) {
+					return false;
+				}
+			}
+			return true;
+		} else {
+			return true;
+		}
+	}
+	private function call($methodOwnerService, $requestObject) {
+		$callbackFunction = array($methodOwnerService,$requestObject->method);
+		return call_user_func_array($callbackFunction, $requestObject->params);
+	}
+	private function doResponse($responseObject) {
+		if(!empty($responseObject)) {
+			header('Content-Type: application/json');
+			echo json_encode($responseObject);
+		}
 	}
 }
 ?>
