deada235b03493f67f26301e8f6b74db3a517208
Knut Erik Langdahl <knut.erik.langdahl@gmail.com>
2012-11-01 13:53:57 +0100

    User Major improvements

--- client/src/my/sheeponthetable/tools/User.java
+++ client/src/my/sheeponthetable/tools/User.java
@@ -12,13 +12,45 @@ public class User {
 
     private String username, name, email, telephone;
     private int userId;
-
+    private boolean SMSAlarmStationary,
+                    SMSAlarmAttack,
+                    SMSAlarmTemperature,
+                    EmailAlarmStationary,
+                    EmailAlarmAttack,
+                    EmailAlarmTemperature;
+    
     public User(int userId, String username, String name, String email, String telephone) {
         this.userId = userId;
         this.username = username;
         this.name = name;
         this.email = email;
         this.telephone = telephone;
+        
+    }
+    public User(    int userId,
+                    String username,
+                    String name,
+                    String email,
+                    String telephone,
+                    boolean SMSAlarmStationary,
+                    boolean SMSAlarmAttack,
+                    boolean SMSAlarmTemperature,
+                    boolean EmailAlarmStationary,
+                    boolean EmailAlarmAttack,
+                    boolean EmailAlarmTemperature) {
+        
+        this.userId = userId;
+        this.username = username;
+        this.name = name;
+        this.email = email;
+        this.telephone = telephone;
+        
+        this.SMSAlarmAttack = SMSAlarmAttack;
+        this.SMSAlarmStationary = SMSAlarmStationary;
+        this.SMSAlarmTemperature = SMSAlarmTemperature;
+        this.EmailAlarmAttack = EmailAlarmAttack;
+        this.EmailAlarmStationary = EmailAlarmStationary;
+        this.EmailAlarmTemperature = EmailAlarmTemperature;
         
     }
 
