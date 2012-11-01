9e448e8a866cfe62449a0a5ed417245786dc197f
Alexander Olsen <alex.j.olsen@gmail.com>
2012-11-01 11:59:35 +0100

    GUI User

--- client/src/my/sheeponthetable/tools/User.java
+++ client/src/my/sheeponthetable/tools/User.java
@@ -13,7 +13,29 @@ public class User {
     private int telephone, clearance;
     
     public User(String name,String password, String email, int telephone,int clearance){
+        this.name = name;
+        this.password = password;
+        if(telephone == 12) {this.telephone = telephone;}
+        if (clearance > -1 && clearance < 5) {this.clearance = clearance;}
+        if(email.contains("@")){this.email = email;}
     }
     public User(String name,String password, int clearance){
+    if (clearance > -1 && clearance < 5) {this.clearance = clearance;}
+    this.name = name;
+    this.password = password;
     }
+    public int getClearance(){
+    return this.clearance;}
+    
+    public int getTelephone(){
+    return this.telephone;}
+    
+    public String getEmail(){
+    return this.email;}
+    
+    public String getName(){
+    return this.name;}
+    
+    public String getPassword(){
+    return this.password;}
 }
