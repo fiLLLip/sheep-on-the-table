7401aa0920de590016edfcc82e2173b6e4d0324c
Alexander Olsen <alex.j.olsen@gmail.com>
2012-11-01 11:40:21 +0100

    GUI Trolls everywhere

--- client/src/my/sheeponthetable/tools/User.java
+++ client/src/my/sheeponthetable/tools/User.java
@@ -9,5 +9,11 @@ package my.sheeponthetable.tools;
  * @author Alex
  */
 public class User {
+    private String name, password, email;
+    private int telephone, clearance;
     
+    public User(String name,String password, String email, int telephone,int clearance){
+    }
+    public User(String name,String password, int clearance){
+    }
 }
