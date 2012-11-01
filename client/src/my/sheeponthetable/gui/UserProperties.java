e691027b1dec83a538b8026df9ef843f25fc6e05
Filip Andre Larsen Tomren <filiptomren@gmail.com>
2012-11-01 16:30:30 +0100

    Populates usersettings panel, and titlebar for mac.

--- client/src/my/sheeponthetable/gui/UserProperties.java
+++ client/src/my/sheeponthetable/gui/UserProperties.java
@@ -17,15 +17,18 @@ public class UserProperties extends javax.swing.JFrame {
 
     SheepPanel sheepPanel;
     User user;
+
     /**
      * Creates new form AddNewSheep
      */
     public UserProperties(SheepPanel sp) {
         initComponents();
         this.sheepPanel = sp;
-        UsernameLabel.setText(WebServiceClient.username);
         user = WebServiceClient.getUserDetails();
-        
+        lblUsername.setText(WebServiceClient.username);
+        txtName.setText(user.getName());
+        txtEmail.setText(user.getEmail());
+        txtPhone.setText(user.getTelephone());
     }
 
     /**
@@ -38,16 +41,16 @@ public class UserProperties extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel8 = new javax.swing.JLabel();
-        NameTxt = new javax.swing.JTextField();
+        txtName = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
-        mailTxt = new javax.swing.JTextField();
-        PhoneTxt = new javax.swing.JTextField();
-        SaveButton = new javax.swing.JToggleButton();
-        CancelButton = new javax.swing.JToggleButton();
-        UsernameLabel = new javax.swing.JLabel();
+        txtEmail = new javax.swing.JTextField();
+        txtPhone = new javax.swing.JTextField();
+        btnSave = new javax.swing.JToggleButton();
+        btnCancel = new javax.swing.JToggleButton();
+        lblUsername = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -59,21 +62,21 @@ public class UserProperties extends javax.swing.JFrame {
 
         jLabel12.setText("Phone:");
 
-        SaveButton.setText("Save");
-        SaveButton.addActionListener(new java.awt.event.ActionListener() {
+        btnSave.setText("Save");
+        btnSave.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                SaveButtonActionPerformed(evt);
+                btnSaveActionPerformed(evt);
             }
         });
 
-        CancelButton.setText("Cancel");
-        CancelButton.addActionListener(new java.awt.event.ActionListener() {
+        btnCancel.setText("Cancel");
+        btnCancel.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                CancelButtonActionPerformed(evt);
+                btnCancelActionPerformed(evt);
             }
         });
 
-        UsernameLabel.setText("Username");
+        lblUsername.setText("Username");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -88,22 +91,22 @@ public class UserProperties extends javax.swing.JFrame {
                     .addComponent(jLabel12))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(UsernameLabel)
-                    .addComponent(NameTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(mailTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(PhoneTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(lblUsername)
+                    .addComponent(txtName, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtPhone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel10)
                 .addContainerGap(26, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(SaveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(CancelButton)
+                .addComponent(btnCancel)
                 .addGap(68, 68, 68))
         );
 
-        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {NameTxt, PhoneTxt, mailTxt});
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {txtEmail, txtName, txtPhone});
 
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -113,40 +116,40 @@ public class UserProperties extends javax.swing.JFrame {
                 .addGap(8, 8, 8)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel9)
-                    .addComponent(UsernameLabel))
+                    .addComponent(lblUsername))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel8)
-                    .addComponent(NameTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel11)
-                    .addComponent(mailTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel12)
-                    .addComponent(PhoneTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtPhone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 25, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(SaveButton)
-                    .addComponent(CancelButton))
+                    .addComponent(btnSave)
+                    .addComponent(btnCancel))
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void CancelButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CancelButtonActionPerformed
+    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
         this.dispose();
-    }//GEN-LAST:event_CancelButtonActionPerformed
+    }//GEN-LAST:event_btnCancelActionPerformed
 
-    private void SaveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SaveButtonActionPerformed
-       /*
-        * Checks if all fields are filled in
-        */
-        if (this.NameTxt.getText().equals("")
-                || this.mailTxt.getText().equals("")
-                || this.PhoneTxt.getText().equals("")) {
+    private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
+        /*
+         * Checks if all fields are filled in
+         */
+        if (this.txtName.getText().equals("")
+                || this.txtEmail.getText().equals("")
+                || this.txtPhone.getText().equals("")) {
             System.out.println("Error should now show up...");
             // final ImageIcon icon = new ImageIcon(getClass().getResource("/my/sheeponthetable/gui/resources/sadSheep.jpg"));
             JOptionPane.showMessageDialog(null, "Please fill in all the required fields.", "Information", JOptionPane.INFORMATION_MESSAGE);
@@ -155,7 +158,7 @@ public class UserProperties extends javax.swing.JFrame {
             /*
              * Checks if email contains "@"
              */
-        } else if (!this.mailTxt.getText().contains("@")) {
+        } else if (!this.txtEmail.getText().contains("@")) {
             System.out.println("Error should now show up...");
             // final ImageIcon icon = new ImageIcon(getClass().getResource("/my/sheeponthetable/gui/resources/sadSheep.jpg"));
             JOptionPane.showMessageDialog(null, "Please enter a correct e-mail address.", "Information", JOptionPane.INFORMATION_MESSAGE);
@@ -163,37 +166,37 @@ public class UserProperties extends javax.swing.JFrame {
             /*
              * Checks if 
              */
-        } else if (this.PhoneTxt.getText().length() != 8) {
+        } else if (this.txtPhone.getText().length() != 8) {
             System.out.println("Error should now show up...");
             JOptionPane.showMessageDialog(null, "Please enter a phone number consisting of 8 numbers!", "Information", JOptionPane.INFORMATION_MESSAGE);
             return;
         } else {
-                          
-                user.setName(this.NameTxt.getText());
-                user.setEmail(this.mailTxt.getText());
-                user.setTelephone(this.PhoneTxt.getText());
-                if (WebServiceClient.setUserDetails(user)) {
-                    this.dispose();
-                } else {
-                    JOptionPane.showMessageDialog(null, "Failed to edit user!", "Information", JOptionPane.ERROR_MESSAGE);
-                }
+
+            user.setName(this.txtName.getText());
+            user.setEmail(this.txtEmail.getText());
+            user.setTelephone(this.txtPhone.getText());
+            if (WebServiceClient.setUserDetails(user)) {
+                this.dispose();
+            } else {
+                JOptionPane.showMessageDialog(null, "Failed to edit user!", "Information", JOptionPane.ERROR_MESSAGE);
+            }
 
         }
-    }//GEN-LAST:event_SaveButtonActionPerformed
+    }//GEN-LAST:event_btnSaveActionPerformed
     /**
      * @param args the command line arguments
      */
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JToggleButton CancelButton;
-    private javax.swing.JTextField NameTxt;
-    private javax.swing.JTextField PhoneTxt;
-    private javax.swing.JToggleButton SaveButton;
-    private javax.swing.JLabel UsernameLabel;
+    private javax.swing.JToggleButton btnCancel;
+    private javax.swing.JToggleButton btnSave;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
-    private javax.swing.JTextField mailTxt;
+    private javax.swing.JLabel lblUsername;
+    private javax.swing.JTextField txtEmail;
+    private javax.swing.JTextField txtName;
+    private javax.swing.JTextField txtPhone;
     // End of variables declaration//GEN-END:variables
 }
